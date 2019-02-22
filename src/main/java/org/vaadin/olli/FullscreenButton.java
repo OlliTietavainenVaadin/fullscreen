3070c5a07e6fce9833c464297f09f7a80cd241ea
Olli Tietäväinen <ollit@vaadin.com>
2019-02-22 15:21:49 +0200

    initial commit

---
+++ src/main/java/org/vaadin/olli/FullscreenButton.java
@@ -0,0 +1,47 @@
+package org.vaadin.olli;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+
+/**
+ * A vaadin-button wrapper that can open or close fullscreen mode.
+ */
+@Tag("fullscreen-button")
+@HtmlImport("fullscreen-button.html")
+public class FullscreenButton extends PolymerTemplate<FullscreenButton.FullscreenButtonModel> {
+
+    @Id("fullscreen-vaadin--button")
+    private Button fullscreenVaadinButton;
+
+    /**
+     * Creates a new FullscreenButton.
+     */
+    protected FullscreenButton() {
+    }
+
+    /**
+     * Creates a new FullscreenButton with a custom caption
+     * @param buttonText caption for the button
+     * @param close button should close fullscreen
+     */
+    public FullscreenButton(String buttonText, boolean close) {
+        setButtonText(buttonText);
+        getModel().setClose(close);
+    }
+
+    /**
+     * This model binds properties between FullscreenButton and fullscreen-button.html
+     */
+    public interface FullscreenButtonModel extends TemplateModel {
+        void setClose(boolean close);
+        boolean getClose();
+    }
+
+    public void setButtonText(String buttonText) {
+        fullscreenVaadinButton.setText(buttonText);
+    }
+}
---
+++ src/main/resources/META-INF/resources/frontend/fullscreen.js
@@ -0,0 +1,25 @@
+var elem = document.documentElement;
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
