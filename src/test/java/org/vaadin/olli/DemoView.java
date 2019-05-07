dd5341d91784bd20daf36cab611902e47a03bec8
Olli Tietäväinen <ollit@vaadin.com>
2019-05-07 18:26:42 +0300

    update to vaadin 14.0.0.alpha3

--- src/test/java/org/vaadin/olli/DemoView.java
+++ src/test/java/org/vaadin/olli/DemoView.java
@@ -1,9 +1,12 @@
 package org.vaadin.olli;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServlet;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 
 @Route("")
 @JavaScript("fullscreen.js")
@@ -12,5 +15,13 @@ public class DemoView extends Div {
     public DemoView() {
         add(new FullscreenButton("open fullscreen", false));
         add(new FullscreenButton("close fullscreen", true));
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "myservlet", asyncSupported = true, initParams = {
+            @WebInitParam(name = "frontend.url.es6", value = "http://mydomain.com/es6/"),
+            @WebInitParam(name = "frontend.url.es5", value = "http://mydomain.com/es5/"),
+            @WebInitParam(name = "bowerMode", value = "true")}
+    )
+    public static class MyServlet extends VaadinServlet {
     }
 }
